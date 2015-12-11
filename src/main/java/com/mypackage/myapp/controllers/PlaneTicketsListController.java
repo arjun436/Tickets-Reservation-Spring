8d9114ad06bac29dd0eac3c4ba7ecabed0251793
spike606 <krystusw@gmail.com>
2015-12-11 14:47:57 +0100

    usuwanie biletu pociagu -> usuwa rowniez z innych tabel

--- src/main/java/com/mypackage/myapp/controllers/PlaneTicketsListController.java
+++ src/main/java/com/mypackage/myapp/controllers/PlaneTicketsListController.java
@@ -1,19 +1,27 @@
 package com.mypackage.myapp.controllers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.mypackage.myapp.domain.PlaneTicket;
+import com.mypackage.myapp.domain.PlaneTicketOrder;
+import com.mypackage.myapp.domain.User;
+import com.mypackage.myapp.service.PlaneTicketOrderService;
 import com.mypackage.myapp.service.PlaneTicketService;
+import com.mypackage.myapp.service.UserService;
 
 @Controller
 @SessionAttributes
@@ -21,7 +29,13 @@ public class PlaneTicketsListController {
 
 	
 	@Autowired
+	UserService userService;
+	
+	@Autowired
 	PlaneTicketService planeTicketService;
+	
+	@Autowired
+	PlaneTicketOrderService planeTicketOrderService;
 	
 	@RequestMapping("/planeTicketsList")
 	public String listPlaneTickets(Map<String, Object> map, HttpServletRequest request) {
@@ -39,7 +53,49 @@ public class PlaneTicketsListController {
 	}
 	@RequestMapping(value = "/deletePlaneTicket/{planeTicketId}", method = ( RequestMethod.GET))
 	public String deletePlaneTicket(@PathVariable("planeTicketId") Integer planeTicketId) {
+		
+	
+		//delete for users
+		List<User> usersList = userService.listUser();
+		
+        for (Iterator it = usersList.iterator(); it.hasNext();) {
+            User candidate = (User)it.next();
+            System.out.println(candidate.getLogin());
+    		List<PlaneTicketOrder> planeTicketOrderList =  new ArrayList<PlaneTicketOrder>(candidate.getPlaneTicketOrder());
+
+    		
+            for (Iterator it2 = planeTicketOrderList.iterator();it2.hasNext();){
+            		PlaneTicketOrder planeTicketOrder = (PlaneTicketOrder)it2.next();
+                    System.out.println(planeTicketOrder.getPlaneTicket().getFlightNumber());
+
+            		if(planeTicketOrder.getPlaneTicket().getId() == planeTicketId){
+            			it2.remove();
+            			Set<PlaneTicketOrder> set = new HashSet<PlaneTicketOrder>(planeTicketOrderList);
+            			candidate.setPlaneTicketOrder(set);
+            			userService.editUser(candidate);
+            		}
+            	
+            }
+          }
+		
+        
+		List<PlaneTicketOrder> planeTicketOrderList2 =  new ArrayList<PlaneTicketOrder>(planeTicketOrderService.listPlaneTicketOrder());
+
+		
+        for (Iterator it3 = planeTicketOrderList2.iterator();it3.hasNext();){
+        		PlaneTicketOrder planeTicketOrder2 = (PlaneTicketOrder)it3.next();
+
+        		if(planeTicketOrder2.getPlaneTicket().getId() == planeTicketId){
+                    System.out.println(planeTicketOrder2.getId());
+
+        			planeTicketOrderService.removePlaneTicketOrder(planeTicketOrder2.getId());
+
+        		}
+        	
+        }
+
 		planeTicketService.removePlaneTicket(planeTicketId);
+
 
 		return "redirect:/planeTicketsList.html";
 	}
--- src/main/java/com/mypackage/myapp/domain/PlaneTicketOrder.java
+++ src/main/java/com/mypackage/myapp/domain/PlaneTicketOrder.java
@@ -1,6 +1,7 @@
 package com.mypackage.myapp.domain;
 
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
