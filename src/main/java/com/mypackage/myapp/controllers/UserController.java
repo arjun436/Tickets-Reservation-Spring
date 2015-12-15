17563928abbea409602d892b42168548e0cc3899
spike606 <krystusw@gmail.com>
2015-12-15 01:11:22 +0100

    dodane wysylanie maila po rejestracji

--- src/main/java/com/mypackage/myapp/controllers/UserController.java
+++ src/main/java/com/mypackage/myapp/controllers/UserController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mypackage.myapp.domain.Mail;
 import com.mypackage.myapp.domain.User;
 import com.mypackage.myapp.domain.UserRole;
 import com.mypackage.myapp.service.UserService;
@@ -39,9 +42,6 @@ public class UserController {
 	public String user(Map<String, Object> map, HttpServletRequest request) {
 
 		int userId = ServletRequestUtils.getIntParameter(request, "userId", -1);
-		
-		
-
 
 		if (userId > 0)
 			map.put("user", userService.getUser(userId));
@@ -56,25 +56,15 @@ public class UserController {
 	@RequestMapping("/usersList")
 	public String listOfUsers(Map<String, Object> map, HttpServletRequest request) {
 
-		// int userId = ServletRequestUtils.getIntParameter(request, "userId",
-		// -1);
-		//
-		// if (userId > 0)
-		// map.put("user", userService.getUser(userId));
-		// else
-		// map.put("user", new User());
-
 		map.put("usersList", userService.listUser());
 
 		return "usersList";
 	}
 
 	@RequestMapping(value = "/addUser", method = { RequestMethod.POST }) // po
-	// wcisnieciu
-	// add
-	// dodane RequestMethod.GET by internacjonalizacja dzialala
+
 	public String addUser(@ModelAttribute("user") User user, BindingResult result, HttpServletRequest request,
-			Map<String, Object> map) {// przyjmujemy
+			Map<String, Object> map) {
 
 		userValidator.validate(user, result);
 
@@ -88,22 +78,26 @@ public class UserController {
 				for (Iterator it = authorities.iterator(); it.hasNext();) {
 					Object candidate = it.next();
 					if (candidate.toString().equals("ROLE_ADMIN")) {
-						
-						System.out.println(user.getCity());
+
 						userService.addUserAdmin(user);// ADMIN DODAJE Z
 														// UPRAWNIENIAMI ADMINA
 														// I USERA
-						System.out.println("xxx");
-					} 
+					}
 					if (candidate.toString().equals("ROLE_ANONYMOUS")) {
-						
+
 						userService.addUser(user);// GOSC MOZE MIEC TYLKO
 						// UPTRAWNIENIA USERA
-						System.out.println("yyy");
 					}
 
 				}
 			}
+
+			// send email
+			ApplicationContext context = new ClassPathXmlApplicationContext("Spring-mail.xml");
+
+			Mail mm = (Mail) context.getBean("mailMail");
+			mm.sendMail("ticktwo.ticktwo@gmail.com", user.getEmail(), "TickTwo Welcome " + user.getLogin() + "!",
+					"Your account was created. Thank You!");
 
 			return "redirect:home.html";
 
@@ -111,17 +105,6 @@ public class UserController {
 
 		return "user";
 
-		
-		
-		
-
-		
-		
-		
-		
-		
-		
-		
 		// z tego kontrolera jestesmy
 		// przekierowani na users.html
 		// }
@@ -147,17 +130,16 @@ public class UserController {
 	public String addUserRole(@ModelAttribute("userRole") UserRole userRole, BindingResult result,
 			HttpServletRequest request, Map<String, Object> map) {
 
-		
 		userRoleValidator.validate(userRole, result);
 
 		if (result.getErrorCount() == 0) {
-		
-		userService.addUserRole(userRole);
 
-		return "redirect:home.html";
-		
+			userService.addUserRole(userRole);
+
+			return "redirect:home.html";
+
 		}
-		
+
 		return "userRole";
 	}
 
